9564b5ff1a567e524f53ce08f4ccbf06d2ba53e1
Teh Kok How <funcoolgeek@gmail.com>
2026-05-26 13:11:14 +0800

    Fix solution and project files. Remove irrelavant nuget packages.

--- src/RabbitMq.Core/PublishResult.cs
+++ src/RabbitMq.Core/PublishResult.cs
@@ -1,21 +1,13 @@
-using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-
 namespace RabbitMq.Core
 {
     public class PublishResult
     {
-        [JsonProperty]
         public bool Success { get; private set; } = false;
-        [JsonProperty]
         public ulong DeliveryTag { get; private set; }
         //
         // Summary:
         //     Whether this acknoledgement applies to one message or multiple messages.
-        [JsonProperty]
         public bool Multiple { get; private set; }
-        [JsonProperty]
         public List<Error> Errors { get; private set; } = new List<Error>();
         public PublishResult(bool success, ulong tag, bool isMultiple, List<Error> errors)
         {
