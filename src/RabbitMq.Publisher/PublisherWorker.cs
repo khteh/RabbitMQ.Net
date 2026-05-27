8a52d59ebdef821926dc4aa3df1ed387bbc4bfd4
Kok How, Teh <funcoolgeek@gmail.com>
2026-05-27 12:06:45 +0800

    Fix publihser worker ExecuteAsync

--- src/RabbitMq.Publisher/PublisherWorker.cs
+++ src/RabbitMq.Publisher/PublisherWorker.cs
@@ -19,7 +19,6 @@ public class PublisherWorker : BackgroundService
     private readonly PublishingProperties _publishingProperties;
     private IMessage _message;
     private IRabbitMqChannel _channel;
-    private bool _isConnected = false;
 
     public PublisherWorker(IHostApplicationLifetime hostApplicationLifetime, ILogger<PublisherWorker> logger, IOptions<RabbitMQConfig> rabbitMqOptions, SharedState sharedState, IRabbitMqConnection connection, IMessage message)
     {
@@ -46,44 +45,37 @@ public class PublisherWorker : BackgroundService
             EnsureDeliveryToQueue = true // This maps to "mandatory" flag of Publish function. False: Silent when there is no subscriber. True: broker will return BasicReturnEventArgs. https://www.rabbitmq.com/dotnet-api-guide.html
         };
     }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation($"Worker {nameof(PublisherWorker)} starts at: {DateTimeOffset.Now}");
-        if (!_isConnected)
+        try
         {
+            _logger.LogInformation($"ConnectionName: {_rabbitMqOptions.ConnectionName}, Endpoint: {_rabbitMqOptions.Endpoint}, Port: {_rabbitMqOptions.Port}, VHost: {_rabbitMqOptions.VHost}, Message: {_message.Message} @ {_message.Timestamp}, Exchange: {_rabbitMqOptions.Exchange}, RoutingKey: {_rabbitMqOptions.RoutingKey}");
             _channel = await _connection.CreateChannel(string.IsNullOrEmpty(_rabbitMqOptions.Exchange) ? "topic_logs" : _rabbitMqOptions.Exchange, "topic", _queueProperties);
-            _isConnected = true;
+            _logger.LogInformation($"");
+            /* This will initially block until a consumer calls Release() after processing a message, increasing the count by 1.
+            * The following call will not block because it the count is 1. Then it will enters the semaphore with the count decremented by one, continue with the while loop.and call WaitAsync again, which will block.
+            */
+            //_message.Timestamp = DateTimeOffset.UtcNow;
+            PublishResult result = await _channel.Publish<IMessage>(_message, _publishingProperties);
+            if (result != null && result.Success && result.Errors == null)
+                _logger.LogInformation($" [x] Sent {_rabbitMqOptions.RoutingKey}: {_message.Message} @ {_message.Timestamp}");
+            else if (result.Errors != null && result.Errors.Any())
+                _logger.LogError($"Publish failed! {result.Errors.First().Code} {result.Errors.First().Description}");
+            else
+                _logger.LogError("Publish failed with unknown error!");
         }
-        while (!stoppingToken.IsCancellationRequested)
-            try
-            {
-                _logger.LogInformation(" [*] Waiting for logs...");
-                /* This will initially block until a consumer calls Release() after processing a message, increasing the count by 1.
-                * The following call will not block because it the count is 1. Then it will enters the semaphore with the count decremented by one, continue with the while loop.and call WaitAsync again, which will block.
-                */
-                _message.Timestamp = DateTimeOffset.UtcNow;
-                PublishResult result = await _channel.Publish<IMessage>(_message, _publishingProperties);
-                if (result != null && result.Success && result.Errors == null)
-                    _logger.LogInformation($" [x] Sent {_rabbitMqOptions.RoutingKey}: {_message.Message} @ {_message.Timestamp}");
-                else if (result.Errors != null && result.Errors.Any())
-                    _logger.LogError($"Publish failed! {result.Errors.First().Code} {result.Errors.First().Description}");
-                else
-                    _logger.LogError("Publish failed with unknown error!");
-
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical($"{nameof(PublisherWorker)}.{nameof(ExecuteAsync)} Exception ! {ex.Message}");
-            }
-        // Clean up when the service is stopping
-        if (_channel != null)
+        catch (Exception ex)
         {
-            _channel.Dispose();
-            _isConnected = false;
+            _logger.LogCritical($"{nameof(PublisherWorker)}.{nameof(ExecuteAsync)} Exception ! {ex.Message}");
         }
-        _logger.LogInformation($"Worker {nameof(PublisherWorker)}.{nameof(ExecuteAsync)} finishes at: {DateTimeOffset.Now}");
-        //_hostAppLifetime.StopApplication();
+        finally
+        {
+            _logger.LogInformation($"Worker {nameof(PublisherWorker)} finishes at: {DateTimeOffset.Now}");
+            if (_channel != null)
+                _channel.Dispose();
+            _hostAppLifetime.StopApplication();
+        }
     }
     public override async Task StopAsync(CancellationToken cancellationToken)
     {
