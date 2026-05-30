98b317b1822257f7c2ce90e788c5f15e9406db9e
Kok How, Teh <funcoolgeek@gmail.com>
2026-05-30 18:58:52 +0800

    Code cleanup

--- src/RabbitMq.Core/MessageConverter.cs
+++ src/RabbitMq.Core/MessageConverter.cs
@@ -9,15 +9,14 @@ public class MessageConverter : JsonConverter<IMessage>
     public override IMessage Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         // Copy the reader to look ahead into the JSON token document
-        using (JsonDocument doc = JsonDocument.ParseValue(ref reader))
-        {
-            var root = doc.RootElement;
-            // Look for a unique property to determine the type
-            if (root.TryGetProperty("Message", out _) && root.TryGetProperty("Timestamp", out _))
-                //return JsonSerializer.Deserialize<TestMessage>(root.GetRawText(), options);
-                return new TestMessage(root.GetProperty("Message").GetString(), root.GetProperty("Timestamp").GetDateTimeOffset());
-            throw new JsonException($"Unknown message type. JSON: {root.GetRawText()}");
-        }
+
+        using JsonDocument doc = JsonDocument.ParseValue(ref reader);
+        JsonElement root = doc.RootElement;
+        // Look for a unique property to determine the type
+        if (root.TryGetProperty("Message", out _) && root.TryGetProperty("Timestamp", out _))
+            //return JsonSerializer.Deserialize<TestMessage>(root.GetRawText(), options);
+            return new TestMessage(root.GetProperty("Message").GetString(), root.GetProperty("Timestamp").GetDateTimeOffset());
+        throw new JsonException($"Unknown message type. JSON: {root.GetRawText()}");
     }
 
     public override void Write(Utf8JsonWriter writer, IMessage value, JsonSerializerOptions options) =>
